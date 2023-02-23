9de82e2d153f05d7696dc376330acc00a297017f
Xephna <36911354+Xephna@users.noreply.github.com>
2023-02-23 23:04:54 +0800

    remove vite & react boiletplate, add R3F canvas

---
+++ src/Experience.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Experience() {
+  const [count, setCount] = useState(0)
+
+  return (<>
+
+    <color args={['#042243']} attach='background' />
+    
+    </>
+  )
+}
+
+export default Experience
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Experience from './Experience'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Canvas
+    // camera={ {
+		// 	fov: 45,
+		// 	near: 0.1,
+		// 	far: 200,
+		// 	position: [ 2.5, 4, 6 ]
+		// } }
+  >
+    <Experience />
+  </Canvas>
+  // </React.StrictMode>,
 )
